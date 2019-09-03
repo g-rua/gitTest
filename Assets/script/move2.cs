7216800eb4181033fa2c06848f6feacbcc8b43a0
yamatatwo <54833848+yamatatwo@users.noreply.github.com>
2019-09-03 16:33:25 +0900

    Update move2.cs

--- Assets/script/move2.cs
+++ Assets/script/move2.cs
@@ -19,6 +19,7 @@ public class move2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //プレイヤー２操作スクリプト
         GetComponent<AnimationControll>().SetOnGround(Ground);
         g = Ground;
         vel = new Vector3(Input.GetAxis("Horizontal2"), 0, Input.GetAxis("Vertical2"));
