229019e5cae32677318a9434f542a5de4b7a56f7
g-rua <42727913+g-rua@users.noreply.github.com>
2019-09-04 15:00:34 +0900

    update game1
    キャラクターの移動アニメーション　ジャンプ完了

--- Assets/script/move2.cs
+++ Assets/script/move2.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class move2 : MonoBehaviour
 {
-    private Vector3 vel;
+    [SerializeField] AnimationControll ac;
+    public Vector3 vel;
+    private float horizontal;
+    private float vertical;
     private float speed = 5f;
     private float jumpPow = 10f;
-    private float velY = 0f;
+    public float velY = 0f;
     public bool g;
     public bool Ground { get; set; }
     // Start is called before the first frame update
@@ -19,16 +22,37 @@ public class move2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //プレイヤー２操作スクリプト
-        GetComponent<AnimationControll>().SetOnGround(Ground);
+        //プレイヤー２操作用
+        ac.SetOnGround(Ground);
+
         g = Ground;
-        vel = new Vector3(Input.GetAxis("Horizontal2"), 0, Input.GetAxis("Vertical2"));
-        transform.position += vel * speed * Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.X) && Ground)
+        horizontal = Input.GetAxis("Horizontal2");
+        vertical = Input.GetAxis("Vertical2");
+        ac.SetWalkAnimation(vertical);
+        //vel.x = horizontal;
+        //vel.z = vertical;
+        
+        if(vel.y<=0f)
         {
-            velY = jumpPow;
-            Ground = false;
+            velY = 0f;
+            vel.y = 0f;
+        }
+        else
+        {
+            velY += -8f*Time.deltaTime;
+            vel.y += velY;
         }
 
+
+
+        if (Input.GetKeyDown(KeyCode.X) && Ground)
+        {
+            Debug.Log("jump");
+            velY =1.13f;
+            Ground = false;
+        }
+        vel.y += velY;
+        transform.position += ((transform.forward ) * (speed * vertical)+vel) * Time.deltaTime;
+        transform.Rotate(Vector3.up, horizontal * 3f);
     }
 }
