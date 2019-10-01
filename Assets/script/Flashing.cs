3324a98eb560c7c1a3a851bb35bf98a4fa31cc94
g-rua <42727913+g-rua@users.noreply.github.com>
2019-10-01 15:22:19 +0900

    爆発による吹っ飛びの変化
    タイトルであそぼー

---
+++ Assets/script/Flashing.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashing : MonoBehaviour
+{
+    private Color defColor;
+    private Color changeColor;
+    private int flashingIndex = 1;
+    private int maxFlashingCount;
+    public float addAlpha;
+    public int defFlashingCount;
+    public int flashingCount;
+    public int targetObjCnt;
+    public bool changeAlpha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashingCount = defFlashingCount;
+        maxFlashingCount = defFlashingCount;
+        defColor = GetComponent<MeshRenderer>().materials[flashingIndex].color;
+        changeColor = defColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //flashingCount++;
+        //changeColor.a = ((float)(flashingCount) / (float)(maxFlashingCount))+0.2f;
+        //if (flashingCount / 60 < 5)
+        //{
+        flashingCount--;
+        if(flashingCount==360)
+        {
+            addAlpha = 0.1f;
+        }
+        if(flashingCount==240)
+        {
+            addAlpha = 0.2f;
+        }
+        if(flashingCount==120)
+        {
+            addAlpha = 0.4f;
+        }
+        if (changeColor.a >= 1)
+        {
+            addAlpha *= -1f;
+        }
+        if (changeColor.a <= 0)
+        {
+            addAlpha *= -1f;
+        }
+        changeColor.a += addAlpha;
+
+
+        //}
+        GetComponent<MeshRenderer>().materials[flashingIndex].color = changeColor;
+
+    }
+
+
+
+    public void SetCountDownTime(int time)
+    {
+        targetObjCnt = time;
+    }
+}
