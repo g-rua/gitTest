f24057dea3d21167b0c0ed3ff7f70a182e57e42d
g-rua <42727913+g-rua@users.noreply.github.com>
2019-10-04 15:18:10 +0900

    大砲とか玉転がし野坂いじったり

---
+++ Assets/script/CanonFire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonFire : MonoBehaviour
+{
+    [SerializeField] GameObject shell;
+    [SerializeField] GameObject body;
+    [SerializeField] GameObject fireDirection;
+    private float rotSpeed;
+    private Vector3 shellVel;
+    public float shellSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotSpeed = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            GameObject s = GameObject.Instantiate(shell, fireDirection.transform.position, fireDirection.transform.rotation);
+            shellVel = fireDirection.transform.forward * shellSpeed;
+            s.GetComponent<Rigidbody>().AddForce(shellVel);
+        }
+
+        transform.Rotate(Vector3.up, rotSpeed * Input.GetAxis("Horizontal"));
+        body.transform.Rotate(Vector3.right, rotSpeed * Input.GetAxis("Vertical"));
+    }
+}
---
+++ Assets/script/RepeatHitBom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatHitBom : MonoBehaviour
+{
+    [SerializeField] GameObject exp;
+    [SerializeField] GameObject expInstance;
+    public int maxTapCount;
+    public int tapCount;
+    public float scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            tapCount++;
+        transform.localScale += new Vector3(scale ,scale ,scale);
+        }
+        if(tapCount>maxTapCount)
+        {
+            exp = GameObject.Instantiate(expInstance, transform.position, Quaternion.identity);
+            exp.transform.localScale = transform.localScale;
+            Destroy(this.gameObject);
+        }
+        GetComponent<RepeatHitBomColor>().TapCountChangeAlpha(tapCount, maxTapCount);
+    }
+}
