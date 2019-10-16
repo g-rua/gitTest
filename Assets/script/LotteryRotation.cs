44ea249cbb4e9c942057fc582573ab72d40754cd
g-rua <42727913+g-rua@users.noreply.github.com>
2019-10-16 11:14:03 +0900

    福引できた（多分）

--- Assets/script/LotteryRotation.cs
+++ Assets/script/LotteryRotation.cs
@@ -6,17 +6,19 @@ public class LotteryRotation : MonoBehaviour
 {
     [SerializeField] GameObject obj;
     [SerializeField] Vector3 axis;
+    private Quaternion InitQuat;
     public float firstRot;
     public float rotSpeed;  //変更可能な回転初速値
     public float decaySpeed;   //減衰速度 要は摩擦
     public bool up;
     public bool right;
     public bool forward;
-    private float rot;          //回転速度
+    public float rot;          //回転速度
 
     // Start is called before the first frame update
     void Start()
     {
+        InitQuat = transform.rotation;
     }
 
     // Update is called once per frame
@@ -27,17 +29,22 @@ public class LotteryRotation : MonoBehaviour
 
     public void LotteryRot()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+
+
+
+
+        if (rot < 1f)
         {
-            if (rot < firstRot)
+            if (transform.rotation.z- InitQuat.z<0.1f)
             {
-                rot += firstRot;    //上のif文で速度制限してます
+                rot = 0f;
             }
         }
-
+        else
+        {
         rot = rot - decaySpeed; //摩擦でゆっくり
-
-        if(rot > 0)
+        }
+        if (rot > 0)
         {
             if (up)
             {
@@ -54,4 +61,18 @@ public class LotteryRotation : MonoBehaviour
         }
 
     }
+
+    public void SetFirstRot(float frot)
+    {
+        firstRot = frot;
+        if (rot < firstRot)
+        {
+            rot += firstRot;    //上のif文で速度制限してます
+        }
+    }
+    public float GetRot()
+    {
+        return rot;
+    }
+
 }
---
+++ Assets/script/LotterySphere.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LotterySphere : MonoBehaviour
+{
+    private int lotteryIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LotteryRandomIndex();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LotteryRandomIndex()
+    {
+        lotteryIndex = Random.Range(0, 4);
+        LotteryIndexChangeColor(lotteryIndex);
+    }
+
+    private void LotteryIndexChangeColor(int index)
+    {
+        Color col;
+        switch (index)
+        {
+            case 0:
+                col = Color.yellow;
+                break;
+            case 1:
+                col = Color.red;
+                break;
+            case 2:
+                col = Color.blue;
+                break;
+            case 3:
+                col = Color.white;
+                break;
+            default:
+                col = Color.white;
+                break;
+        }
+        GetComponent<MeshRenderer>().materials[0].color = col;
+
+    }
+
+}
