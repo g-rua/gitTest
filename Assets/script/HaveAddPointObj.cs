b7b4555254f0e367b97d38089c69d799fdca79d2
g-rua <42727913+g-rua@users.noreply.github.com>
2019-10-23 13:55:21 +0900

    アセット追加 あと一個ミニゲーム追加
    持っている間ポイント増えて逃げるやつ

---
+++ Assets/script/HaveAddPointObj.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaveAddPointObj : MonoBehaviour
+{
+    public GameObject carrier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.parent != null)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            transform.parent.GetComponent<HaveAddPoint>().point++;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player"&&transform.parent==null)
+        {
+            carrier = other.gameObject;
+            transform.position = other.gameObject.transform.Find("AddPointPos").position;
+            transform.parent = other.gameObject.transform.Find("AddPointPos");
+            
+        }
+    }
+
+}
