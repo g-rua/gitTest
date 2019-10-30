f909fb20b1e69a64e57ee3d61bed1ae5ce5a5c6f
g-rua <42727913+g-rua@users.noreply.github.com>
2019-10-30 15:11:13 +0900

    操作分割
    もしかしたら巻き戻すかも

--- Assets/script/P2InputControll.cs
+++ Assets/script/P2InputControll.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class P2InputControll : MonoBehaviour
 {
     [SerializeField] CharaCon cc;
+    [SerializeField] CarryItemAction itemAction;
+    private GameObject haveObj;
+    private bool Ground;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +17,45 @@ public class P2InputControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //jumpFlame = cc.jumpFlame;
+        haveObj =cc.haveObj;
+        Ground = cc.g;
         cc.SetDirection(Input.GetAxis("Vertical2"),Input.GetAxis("Horizontal2"));
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            cc.ItemCarry();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            itemAction.ItemAction(haveObj);
+        }
+
+        //ジャンプの区別
+        if (Input.GetKey(KeyCode.X) && Ground)
+        {
+            //小ジャンプか大ジャンプを区別するためにインクリメント
+            cc.jumpFlame++;
+
+            //一定値以上になったら強制ジャンプ
+            if (cc.jumpFlame >= cc.maxJumpFlame)
+            {
+                cc.DecideJumpPower(cc.jumpFlame);
+                cc.jumpFlame = 0;
+            }
+        }
+
+        //即離した場合
+        if (Input.GetKeyUp(KeyCode.X) && Ground)
+        {
+            Debug.Log("harf");
+            cc.DecideJumpPower(cc.jumpFlame);
+            cc.jumpFlame = 0;
+        }
+
     }
+
 
 
 
